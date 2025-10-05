4f8dac56e3a37d73a0b0635aa8ee0de0eecc4c87
Mensagem Do Carinho <mensagemdocarinho@gmail.com>
2025-10-05 21:30:08 +0000

    Primeiro commit do projeto Firebase

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQyFGHkHLX1ScMB5ntjSXf5tfkQ7zyQhQ",
+  authDomain: "studio-7822979176-66da4.firebaseapp.com",
+  projectId: "studio-7822979176-66da4",
+  storageBucket: "studio-7822979176-66da4.firebasestorage.app",
+  messagingSenderId: "1065389918249",
+  appId: "1:1065389918249:web:d6014e4c0a44de7dc2f438"
+};
+
+// Initialize Firebase
+import { initializeApp } from "firebase/app"; // Verifique essa importação
+
